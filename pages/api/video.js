5f01ab15f6f165951d833e6bc29057c5c78a524a
Andrew Player <andrewplayer3@gmail.com>
2022-03-16 15:51:16 -0800

    basic video schema and api files

---
+++ pages/api/video.js
@@ -0,0 +1,34 @@
+import connectDB from '../../middleware/mongodb';
+import Video from '../../models/video';
+
+const handler = async (req, res) => {
+    if (req.method === 'POST') {   // Set Video Information
+        const { title, storage_location, length, resolution, description, tags } = req.body;  
+        try {            
+            var video = new Video({
+                title: title,
+                location: storage_location,
+                description: description,
+                tags: tags,
+                metadata: {
+                    length: length,
+                    resolution: resolution
+                },
+                analytics: {
+                    total_rating: 0,
+                    num_ratings: 0,
+                    views: 0
+                }
+            });
+            // Create new video entry
+            var videocreated = await video.save();
+            return res.status(200).send(videocreated);
+        } catch (error) {
+            return res.status(500).send(error.message);
+        }
+    } else if (req.method === 'GET') {  // Retrieve Video Information
+
+    }
+};
+
+export default connectDB(handler);
