0a6fc797e037c367a8cfdf1942e7d19a402ee56e
cpurev <cpurev@alaska.com>
2022-03-17 18:07:26 -0800

    Navigation Bar

---
+++ components/common/Header/Header.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import config from '../../../config/meta.json'
+
+export default function Header(){
+    return(
+    <div>
+        <Head>
+            <title key="title">
+                {config.title}
+            </title>
+        </Head>
+    </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
-import NavBar from './navbar.js'
+import NavBar from '../components/navbar.js'
 
 export default function Home() {
   return (
