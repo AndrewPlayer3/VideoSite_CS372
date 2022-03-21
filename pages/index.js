6a1377fd039b1a4091ba26d20acb1d4c7c35e995
Andrew Player <andrewplayer3@gmail.com>
2022-03-21 15:50:53 -0800

    index page with basic player

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
 
 import NavBar from '../components/navbar.js'
 import Footer from '../components/common/Footer'
+import { useSession, getSession } from "next-auth/react"
+import ReactPlayer from 'react-player/file';
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  }
+}
 
 export default function Home() {
-  return (
-    <>
-      <NavBar />
-      <Footer />
-    </>
-  )
+    
+    const { data: session, status } = useSession()
+
+    console.log(JSON.stringify(session));
+
+    if (session) {
+        return (
+            <div>
+                <NavBar />
+                <div className='player-box'>
+                    <ReactPlayer controls url='chickens.mp4' />
+                </div>
+                <Footer />
+            </div>
+        );
+    } 
+    return <a href='/api/auth/signin'>Signin to Continue</a>
 }
