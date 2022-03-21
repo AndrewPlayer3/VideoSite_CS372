2280940738c38141367004d5e3ea086c1856d3c6
Andrew Player <andrewplayer3@gmail.com>
2022-03-21 15:50:46 -0800

    user schema

---
+++ models/user.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        viewer: {  // Everyone should be a viewer, so this isn't strictly necessary.
+            type: Boolean,
+            default: true
+        },
+        content_editor: Boolean,
+        content_manager: Boolean
+    }
+});
+
+mongoose.models = {};
+
+var User = mongoose.model('User', user);
+
+export default User;
