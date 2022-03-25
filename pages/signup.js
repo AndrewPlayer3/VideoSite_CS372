3699e90e7045efca21617bc9e6627b3e72c0dbaf
Andrew Player <andrewplayer3@gmail.com>
2022-03-25 07:16:37 -0800

    Signup moved to component

--- pages/signup.js
+++ pages/signup.js
@@ -1,18 +1,9 @@
+import SignUpForm from '../components/SignUpForm';
+
 export default function SignUp() {
   return (
     <>
-        <form action="/api/user" method="POST">
-            <label for="uname">Username:</label>
-            <input name="uname" type="text" placeholder="Enter Username" required></input>
-
-            <label for="pwd">Password:</label>
-            <input name="pwd" type="password" placeholder="Enter Password" required></input>
-
-            <label for="email">Email:</label>
-            <input name="email" type="email" placeholder="Enter Email" required></input>
-
-            <button type="submit">Login</button>
-        </form>
+      <SignUpForm />
     </>
   )
 }
