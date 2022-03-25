b6fa7f04aab3aa012d27fd3063f5c6b1d0e2eeb1
Andrew Player <andrewplayer3@gmail.com>
2022-03-25 07:15:00 -0800

    Navbar Styling

--- components/common/Navbar/Navbar.js
+++ components/common/Navbar/Navbar.js
@@ -9,8 +9,15 @@ function classNames(...classes) {
 }
 
 export default function NavBar() {
+
+  // orange #D77A61   accents
+  // blue   #223843   background
+  // white  #EFF1F3   background
+  // grey   #DBD3D8   background
+  // peach  #D8B4A0   accent
+
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-[#223843]">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto ">
@@ -18,20 +25,27 @@ export default function NavBar() {
               <div className="flex items-center justify-start mr-6 sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center cursor-pointer">
                   <Link href='/'>
-                    <img
-                      className="hidden lg:block h-8 w-auto"
-                      src="favicon.ico"
-                      alt="Logo"
-                    />
+                    <div className='flex h-auto w-auto'>
+                      <div className='ml-1 items-center'>
+                        <img
+                          className="flex items-start justify-start hidden lg:block h-8 w-auto"
+                          src="movie-film.svg"
+                          alt="Logo"
+                        />
+                      </div>
+                      <div className='flex items-center justify-end text-xl text-[#EFF1F3] ml-1 mr-1'>
+                        <h1 className="font-sans">Fletnix</h1>
+                      </div>
+                    </div>
                   </Link>
                 </div>
               </div>
               {/* Search Bar and Button */}
-              <div className="flex-1 flex items-center justify-start sm:items-stretch sm:justify-start">
-                <div className="py-10 w-full">
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex items-center justify-center py-10 w-full">
                   <div className="input-group relative flex items-center w-5/6">
-                  <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-600 transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2"></input>
-                    <button class="btn inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
+                  <input type="search" class="form-control relative flex items-center justify-center min-w-0 block w-full px-3 py-1.5 text-base font-normal text-[#223843] bg-[#364B54] bg-clip-padding rounded-lg border border-solid border-[#0A2730] transition ease-in-out mr-1 m-0 focus:text-gray-700 focus:bg-[#EFF1F3] focus:border-blue-600 focus:outline-none" aria-label="Search" aria-describedby="button-addon2"></input>
+                    <button class="btn rounded-lg inline-block px-6 py-2.5 bg-[#D77A61] text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-[#D8B4A0] hover:shadow-lg focus:bg-[#D8B4A0]  focus:shadow-lg border border-solid border-[#0A2730] focus:outline-none focus:ring-0 active:bg-[#D8B4A0] active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
                       <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="w-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                       </svg>
@@ -44,7 +58,7 @@ export default function NavBar() {
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative">
                   <div>
-                    <Menu.Button className="bg-gray-800 text-gray-400 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <Menu.Button className="text-[#EFF1F3] flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open user menu</span>
                       <UserIcon className="h-6 w-6" aria-hidden="true"/>
                     </Menu.Button>
