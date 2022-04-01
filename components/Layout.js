dd6b5f30731d60a5783d86ae053f292055a2aa7c
Andrew Player <andrewplayer3@gmail.com>
2022-04-01 12:23:30 -0800

    better scrolling

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Header from "../components/common/Header"
 
 export default function Layout({children, pageProps}){
     return(
-        <div className='flex h-screen max-w-screen'> 
+        <div className='flex max-w-screen'> 
             <div className="fixed top-0 w-screen drop-shadow-lg z-40">
                 <NavBar liveSearch = { true } />
             </div>
