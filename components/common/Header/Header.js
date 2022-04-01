d20bb36aebff6c35319556746e78286f2b1797df
Andrew Player <andrewplayer3@gmail.com>
2022-03-31 16:38:27 -0800

    favicon updated

--- components/common/Header/Header.js
+++ components/common/Header/Header.js
@@ -9,7 +9,7 @@ export default function Header(){
                 {config.title}
             </title>
             <meta name="keywords" content="video streaming site" />
-            <link rel="shortcut icon" href="/favicon.svg" />
+            <link rel="shortcut icon" href="/favicon.png" />
         </Head>
     </div>
     )
