5ddac6a725c5f4a595c967e06c17466328e61e83
Andrew Player <andrewplayer3@gmail.com>
2022-04-04 13:21:00 -0800

    Hiding unused sidebar links

--- components/common/Sidebar/Sidebar.js
+++ components/common/Sidebar/Sidebar.js
@@ -36,7 +36,7 @@ function Sidebar() {
                     </svg>} label="Content"/>
                     
 
-        <LinkAction href="/dashboard/analytics"
+        {/* <LinkAction href="/dashboard/analytics"
                     svg={<svg className="fill-current text-slate-200 w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 0 24 24">
                         <path className="heroicon-ui"
@@ -55,7 +55,7 @@ function Sidebar() {
                               viewBox="0 0 24 24">
                         <path className="heroicon-ui"
                               d="M12 22a10 10 0 110-20 10 10 0 010 20zm0-2a8 8 0 100-16 8 8 0 000 16zM10.59 8.59a1 1 0 11-1.42-1.42 4 4 0 115.66 5.66l-2.12 2.12a1 1 0 11-1.42-1.42l2.12-2.12A2 2 0 0010.6 8.6zM12 18a1 1 0 110-2 1 1 0 010 2z"/>
-                    </svg>} label="Support"/>
+                    </svg>} label="Support"/> */}
 
     </nav>;
 }
