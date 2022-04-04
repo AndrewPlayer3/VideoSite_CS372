1f55ab34c22d6de3c82cf5ad9d9e342373d3c610
Andrew Player <andrewplayer3@gmail.com>
2022-04-04 13:21:26 -0800

    removed newlines

--- pages/api/rate.js
+++ pages/api/rate.js
@@ -7,22 +7,17 @@ const handler = async (req, res) => {
     if (req.method === 'POST') {   // Set Video Information | TODO: Only CONTENT_EDITORs should be able to make these requests.
 
         const session = await getSession({ req });
-
         const { user } = session; 
-
         const { rating, id } = JSON.parse(req.body);
 
         try {
-
             const ratings = await User.findOne({email: user.email, ratings: {"$elemMatch": {video_id: id}}});
-
             if (!ratings) {
                 const user_info = await User.findOneAndUpdate({email: user.email}, {"$push": {ratings: {video_id: id, rating: rating}}});
             } else {
                 const like_video = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.total_rating': -rating } });
                 const inc_likes  = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.num_ratings' : -1 } });
             }
-
             try {
                 const like_video = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.total_rating': rating } });
                 const inc_likes  = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.num_ratings' : 1 } });
