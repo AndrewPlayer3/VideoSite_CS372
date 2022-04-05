862a2991c84f8493e7122f0df8eb1dce8dbac2ea
Andrew Player <andrewplayer3@gmail.com>
2022-04-05 04:02:33 -0800

    cleaned video api

--- pages/api/video.js
+++ pages/api/video.js
@@ -41,33 +41,37 @@ const handler = async (req, res) => {
             return res.status(500).send(error.message);
         }
     } else if (req.method === 'GET') {  // Retrieve Video Information | TODO: Restrict these requests to any logged-in user.
-        const { text_query, tag } = req.query;
-        let id = "";
-        if (req.query.id) {
-            id = req.query.id;
-        }
-        if (text_query) {
-            const query_results = await Video.find({$or: [{ "title": { $regex: text_query, $options: 'i' } }, { "tags": text_query }] });
-            if (query_results) {
-                return res.status(200).send(query_results);
-            } else {
-                return res.status(404).send('No Videos Found.');
+        try {
+            const { text_query, tag } = req.query;
+            let id = "";
+            if (req.query.id) {
+                id = req.query.id;
             }
-        } else if (id) {
-            const query_results = await Video.findById(id);
-            if (query_results) {
-                console.log(query_results);
-                return res.status(200).send(query_results);
+            if (text_query) {
+                const query_results = await Video.find({$or: [{ "title": { $regex: text_query, $options: 'i' } }, { "tags": text_query }] });
+                if (query_results) {
+                    return res.status(200).send(query_results);
+                } else {
+                    return res.status(404).send('No Videos Found.');
+                }
+            } else if (id) {
+                const query_results = await Video.findById(id);
+                if (query_results) {
+                    console.log(query_results);
+                    return res.status(200).send(query_results);
+                } else {
+                    return res.status(404).send('No Videos Found.');
+                }
             } else {
-                return res.status(404).send('No Videos Found.');
+                const query_results = await Video.find({});
+                if (query_results) {
+                    return res.status(200).send(query_results);
+                } else {
+                return res.status(500).send('No Videos Found.'); 
+                }
             }
-        } else {
-            const query_results = await Video.find({});
-            if (query_results) {
-                return res.status(200).send(query_results);
-            } else {
-               return res.status(500).send('No Videos Found.'); 
-            }
+        } catch (error) {
+            return res.status(500).send({videos: [], error: error.message})
         }
     } else if (req.method == 'DELETE') {
         const user_res = await fetch('http://localhost:3000/api/user', {
@@ -81,9 +85,7 @@ const handler = async (req, res) => {
         if (!user_data.role.content_editor) {
             return res.status(403).send('Only Content Editors can Remove Videos.');  
         }
-    
         const { id } = JSON.parse(req.body);
-
         if (id) {
             try {
                 const video_removed = await Video.deleteOne({_id: id});
@@ -100,12 +102,11 @@ const handler = async (req, res) => {
             const view_video = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.views': 1 } });
             res.status(200).send({viewed: true})
         } catch (error) {
-            res.status(500).send({message: 'Error putting view'});
+            res.status(500).send({viewed: false, message: 'Error putting view'});
         }
     } else {
         res.status(422).send('Invalid Request.');
     }
-    // TODO: Delete Request (Based off of Mongo ID for Video)
 };
 
 export default connectDB(handler);
